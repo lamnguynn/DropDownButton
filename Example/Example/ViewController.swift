8728bdee9e227fdfbd15034d58f924f86686ca64
Lam Nguyen <lam@Lams-Mac-mini.local>
2021-08-14 12:56:38 -0500

    Updated README

---
+++ Example/Example/ViewController.swift
@@ -0,0 +1,24 @@
+//
+//  ViewController.swift
+//  Example
+//
+//  Created by Lam Nguyen on 8/14/21.
+//
+
+import UIKit
+import DropDownButton
+
+class ViewController: UIViewController {
+    
+    let dataSource: [String] = ["Apples", "Oranges", "Bananas"]
+    let dropDownButton: DropDownButton? = DropDownButton()
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        //Set up the drop down button
+    }
+
+
+}
+
---
+++ Example/Pods/Target Support Files/DropDownButton/DropDownButton-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_DropDownButton : NSObject
+@end
+@implementation PodsDummy_DropDownButton
+@end
