7c30f1c6611129cef31fd614ddb4591492ed8bec
Lam Nguyen <lam@Lams-Mac-mini.local>
2021-08-14 13:34:28 -0500

    Updated Example Project

--- Example/Example/ViewController.swift
+++ Example/Example/ViewController.swift
@@ -15,8 +15,25 @@ class ViewController: UIViewController {
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        
+        self.view.backgroundColor = .gray
+        self.view.addSubview(dropDownButton!)
         //Set up the drop down button
+        dropDownButton?.dataSource = self.dataSource
+        dropDownButton?.setTitle("Select a food", for: .normal)
+        dropDownButton?.backgroundColor = .systemIndigo
+        
+        dropDownButton!.dropDownTextSize = 16
+        dropDownButton!.heightOfDropDown = 200
+        dropDownButton?.dataSource = self.dataSource
+        dropDownButton?.dropDownColor = .systemIndigo
+        dropDownButton?.dropDownTextColor = .lightGray
+        dropDownButton?.dropDownCornerRadius = 25
+        
+        dropDownButton?.translatesAutoresizingMaskIntoConstraints = false
+        dropDownButton?.widthAnchor.constraint(equalToConstant: 150).isActive = true
+        dropDownButton?.heightAnchor.constraint(equalToConstant: 45).isActive = true
+        dropDownButton?.topAnchor.constraint(equalTo: self.view.topAnchor, constant: 100).isActive = true
+        dropDownButton?.centerXAnchor.constraint(equalTo: self.view.centerXAnchor).isActive = true
     }
 
 
